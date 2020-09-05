027552ded3060db75adb9f77baefbc025e25cbdf
Ahmed Fathi <ahmedfathi680@gmail.com>
2020-09-05 19:49:29 +0200

    Initial Commit

---
+++ FruitBasket/FruitBasket.h
@@ -0,0 +1,19 @@
+//
+//  FruitBasket.h
+//  FruitBasket
+//
+//  Created by Ahmed Fathi on 9/5/20.
+//  Copyright © 2020 Fathi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FruitBasket.
+FOUNDATION_EXPORT double FruitBasketVersionNumber;
+
+//! Project version string for FruitBasket.
+FOUNDATION_EXPORT const unsigned char FruitBasketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FruitBasket/PublicHeader.h>
+
+
